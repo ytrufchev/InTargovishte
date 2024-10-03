8dcc7cc98a1f655ef11e16565a94b68c4544ae8d
ytrufchev <yordan.tihomirov@gmail.com>
2024-10-03 10:49:52 +0300

    code cleanup

--- src/main/java/eu/trufchev/intargovishte/security/JwtTokenProvider.java
+++ src/main/java/eu/trufchev/intargovishte/security/JwtTokenProvider.java
@@ -3,8 +3,6 @@ package eu.trufchev.intargovishte.security;
 import io.jsonwebtoken.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
