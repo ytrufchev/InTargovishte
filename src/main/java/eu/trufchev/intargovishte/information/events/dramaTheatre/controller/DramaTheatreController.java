5fd961f6b04035da7507784e591e8575d945f5d2
ytrufchev <yordan.tihomirov@gmail.com>
2024-10-10 10:42:54 +0300

    typo fix

--- src/main/java/eu/trufchev/intargovishte/information/events/dramaTheatre/controller/DramaTheatreController.java
+++ src/main/java/eu/trufchev/intargovishte/information/events/dramaTheatre/controller/DramaTheatreController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RequestMapping("content/plays")
+@RequestMapping("/content/plays")
 @RestController
 public class DramaTheatreController {
     @Autowired
