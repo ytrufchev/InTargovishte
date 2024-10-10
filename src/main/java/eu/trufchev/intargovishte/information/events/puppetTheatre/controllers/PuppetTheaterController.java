d36a1072e4d4e37380b198934c38afa488972cd0
ytrufchev <yordan.tihomirov@gmail.com>
2024-10-10 09:50:24 +0300

    puppet theater mapping change

--- src/main/java/eu/trufchev/intargovishte/information/events/puppetTheatre/controllers/PuppetTheaterController.java
+++ src/main/java/eu/trufchev/intargovishte/information/events/puppetTheatre/controllers/PuppetTheaterController.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("content/puppettheater")
+@RequestMapping("/puppettheater")
 public class PuppetTheaterController {
 
     @Autowired
