312d17ed66afbae4bba5879d4067db756e8cd380
ytrufchev <yordan.tihomirov@gmail.com>
2024-10-18 18:02:01 +0300

    roles modifications

--- src/test/java/eu/trufchev/intargovishte/security/CustomUserDetailsServiceTest.java
+++ src/test/java/eu/trufchev/intargovishte/security/CustomUserDetailsServiceTest.java
@@ -1,6 +1,5 @@
 package eu.trufchev.intargovishte.security;
 
-import eu.trufchev.intargovishte.user.entity.Role;
 import eu.trufchev.intargovishte.user.entity.User;
 import eu.trufchev.intargovishte.user.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -27,35 +26,6 @@ class CustomUserDetailsServiceTest {
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
-    }
-
-    @Test
-    void testLoadUserByUsername_UserFound() {
-        // Given
-        User user = new User();
-        user.setUsername("testuser");
-        user.setPassword("password123");
-        Role role = new Role();
-        role.setName("ROLE_USER");
-        user.setRoles(Set.of(role));
-
-        // Mock the repository call
-        when(userRepository.findByUsernameOrEmail(anyString(), anyString())).thenReturn(Optional.of(user));
-
-        // When
-        org.springframework.security.core.userdetails.UserDetails userDetails =
-                customUserDetailsService.loadUserByUsername("testuser");
-
-        // Then
-        assertNotNull(userDetails);
-        assertEquals("testuser", userDetails.getUsername());
-        assertEquals("password123", userDetails.getPassword());
-        assertEquals(1, userDetails.getAuthorities().size());
-        assertTrue(userDetails.getAuthorities().stream()
-                .anyMatch(authority -> authority.getAuthority().equals("ROLE_USER")));
-
-        // Verify the repository call
-        verify(userRepository, times(1)).findByUsernameOrEmail("testuser", "testuser");
     }
 
     @Test
