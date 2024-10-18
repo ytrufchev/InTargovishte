9e1dde8535341e4e825f4255467d01a816d44add
ytrufchev <yordan.tihomirov@gmail.com>
2024-10-18 17:25:31 +0300

    roles modifications

--- src/main/java/eu/trufchev/intargovishte/user/entity/Role.java
+++ src/main/java/eu/trufchev/intargovishte/user/entity/Role.java
@@ -11,7 +11,6 @@ import java.util.Collections;
 import java.util.Set;
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
 public class Role {
     public static final String USER = "ROLE_USER";
