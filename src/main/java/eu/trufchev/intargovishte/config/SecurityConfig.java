f3b7930c9336d0d8a6a68cac1a6de31d7ce41035
ytrufchev <yordan.tihomirov@gmail.com>
2024-10-21 09:06:57 +0300

    security chain modifications

--- src/main/java/eu/trufchev/intargovishte/config/SecurityConfig.java
+++ src/main/java/eu/trufchev/intargovishte/config/SecurityConfig.java
@@ -40,8 +40,7 @@ public class SecurityConfig {
                     authorize.requestMatchers("/auth/**").permitAll();
                     authorize.requestMatchers("/stats/**").permitAll();
                     authorize.requestMatchers("/superadmin/**").hasAuthority("ROLE_SUPERADMIN");
-                    authorize.requestMatchers("/content/**/update").hasAuthority("ROLE_SUPERADMIN");
-                    authorize.requestMatchers("/information/**/update").hasAuthority("ROLE_SUPERADMIN");
+                    authorize.requestMatchers("/**").hasAuthority("ROLE_SUPERADMIN");
                     authorize.requestMatchers("/content/**/all").authenticated();
                     authorize.requestMatchers("/information/**/all").authenticated();
                     authorize.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll();
