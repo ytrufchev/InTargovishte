8d815ad961bb960d6f630d642eb9b0f61368cf05
ytrufchev <yordan.tihomirov@gmail.com>
2024-10-22 19:23:21 +0300

    fuel price update functionality integration

--- src/main/java/eu/trufchev/intargovishte/information/fuelo/GasStationController.java
+++ src/main/java/eu/trufchev/intargovishte/information/fuelo/GasStationController.java
@@ -2,7 +2,10 @@ package eu.trufchev.intargovishte.information.fuelo;
 
 import eu.trufchev.intargovishte.information.fuelo.entities.FuelPrice;
 import eu.trufchev.intargovishte.information.fuelo.entities.GasStation;
+import eu.trufchev.intargovishte.information.fuelo.entities.GasstationsList;
+import eu.trufchev.intargovishte.information.fuelo.feignclient.FueloClient;
 import eu.trufchev.intargovishte.information.fuelo.repository.GasStationRepository;
+import eu.trufchev.intargovishte.information.fuelo.services.ParseGasStationToHtml;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,7 +19,24 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class GasStationController {
 
+    private final FueloClient fueloClient;
+    private final ParseGasStationToHtml parseGasStationToHtml;
     private final GasStationRepository gasStationRepository;
+
+    @GetMapping("/update")
+    public ResponseEntity<List<GasStation>> manualUpdate(){
+        return updateGasStations();
+    }
+    public ResponseEntity<List<GasStation>> updateGasStations() {
+        GasstationsList gasstationsLists = new GasstationsList();
+        List<GasStation> gasStations = new ArrayList<>();
+        for(int i = 0; i < gasstationsLists.getGasstations().size(); i++){
+            gasStations.add(parseGasStationToHtml.parseGasStationHtml(fueloClient.getGasstationDetails(gasstationsLists.getGasstations().get(i), "bg")));
+        }
+        gasStationRepository.deleteAll();
+        gasStationRepository.saveAll(gasStations);
+        return ResponseEntity.ok(gasStations);
+    }
 
     @GetMapping("/all")
     public ResponseEntity<List<GasStation>> getAllFuelStations() {
