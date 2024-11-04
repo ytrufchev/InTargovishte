c30f42037c0049e7e1a6cbbe69a65234ac0687c8
ytrufchev <yordan.tihomirov@gmail.com>
2024-11-04 15:23:31 +0200

    avatar update

--- src/main/java/eu/trufchev/intargovishte/user/dto/AvatarUpdateDto.java
+++ src/main/java/eu/trufchev/intargovishte/user/dto/AvatarUpdateDto.java
@@ -5,6 +5,6 @@ import lombok.Data;
 
 @Data
 public class AvatarUpdateDto {
-    @Size(max = 50000, message = "Avatar image data must be 50000 characters or less")
+    @Size(max = 150000, message = "Avatar image data must be 50000 characters or less")
     private String avatarBase64;
 }
