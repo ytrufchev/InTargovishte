26ef5b7c039db52ddd7ace234fe2aa4703b134d7
ytrufchev <yordan.tihomirov@gmail.com>
2024-11-05 10:55:45 +0200

    user deletion for superadmin

--- src/main/java/eu/trufchev/intargovishte/user/repository/UserRepository.java
+++ src/main/java/eu/trufchev/intargovishte/user/repository/UserRepository.java
@@ -5,6 +5,7 @@ import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -13,8 +14,8 @@ public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findByUsernameOrEmail(String username, String email);
     User findByUsername(String username);
     Boolean existsByUsername(String username);
+
     @Modifying
-    @Transactional
-    @Query("DELETE FROM user_roles ur WHERE ur.user_id = :userId")
-    void deleteUserRolesByUserId(Long userId);
+    @Query("DELETE FROM UserRole ur WHERE ur.user.id = :userId")
+    void deleteUserRolesByUserId(@Param("userId") Long userId);
 }
