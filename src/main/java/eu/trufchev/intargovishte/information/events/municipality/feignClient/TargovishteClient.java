63ba2557e3242243a454e71b5b0838531fcb5868
ytrufchev <yordan.tihomirov@gmail.com>
2024-11-27 12:44:12 +0200

    municipality events automation

--- src/main/java/eu/trufchev/intargovishte/information/events/municipality/feignClient/TargovishteClient.java
+++ src/main/java/eu/trufchev/intargovishte/information/events/municipality/feignClient/TargovishteClient.java
@@ -1,50 +1,14 @@
 package eu.trufchev.intargovishte.information.events.municipality.feignClient;
 
-import eu.trufchev.intargovishte.FeignConfigMunicipalityOnly;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-
-@FeignClient(name = "targovishteClient", url = "https://targovishte.bg", configuration = FeignConfigMunicipalityOnly.class)
+@FeignClient(
+        name = "targovishteClient",
+        url = "https://targovishte.bg"
+)
 public interface TargovishteClient {
 
-    @GetMapping("/customSearchWCM/query")
-    String getMunicipalityEvents(@RequestHeader("Accept") String accept,
-                                 @RequestHeader("Accept-Encoding") String acceptEncoding,
-                                 @RequestHeader("Accept-Language") String acceptLanguage,
-                                 @RequestHeader("Connection") String connection,
-                                 @RequestHeader("Host") String host,
-                                 @RequestHeader("Referer") String referer,
-                                 @RequestHeader("Sec-Ch-Ua") String secChUa,
-                                 @RequestHeader("Sec-Ch-Ua-Mobile") String secChUaMobile,
-                                 @RequestHeader("Sec-Ch-Ua-Platform") String secChUaPlatform,
-                                 @RequestHeader("Sec-Fetch-Dest") String secFetchDest,
-                                 @RequestHeader("Sec-Fetch-Mode") String secFetchMode,
-                                 @RequestHeader("Sec-Fetch-Site") String secFetchSite,
-                                 @RequestHeader("User-Agent") String userAgent,
-                                 @RequestParam("context") String context,
-                                 @RequestParam("libName") String libName,
-                                 @RequestParam("saId") String saId,
-                                 @RequestParam("atId") String atId,
-                                 @RequestParam("returnElements") List<String> returnElements,
-                                 @RequestParam("rootPage") String rootPage,
-                                 @RequestParam("returnProperties") List<String> returnProperties,
-                                 @RequestParam("rPP") int rPP,
-                                 @RequestParam("currentPage") int currentPage,
-                                 @RequestParam("currentUrl") String currentUrl,
-                                 @RequestParam("dateFormat") String dateFormat,
-                                 @RequestParam("ancestors") boolean ancestors,
-                                 @RequestParam("descendants") boolean descendants,
-                                 @RequestParam("orderBy") String orderBy,
-                                 @RequestParam("orderBy2") String orderBy2,
-                                 @RequestParam("orderBy3") String orderBy3,
-                                 @RequestParam("sortOrder") boolean sortOrder,
-                                 @RequestParam("searchTerm") String searchTerm,
-                                 @RequestParam("from") String from,
-                                 @RequestParam("before") String before,
-                                 @RequestParam("filterByProperties") String filterByProperties,
-                                 @RequestParam("catId") String catId);
+    @GetMapping(value = "/customSearchWCM/query?context=targovishte.bg1005&libName=content&saId=98cf5967-2cf7-4bda-9bf5-f41a08cc073a&atId=cf1370df-43a1-4012-ba01-85607cf3fe92&returnElements=body,summary,startEventDate,endEventDate,eventType&rootPage=municipality-targovishte&returnProperties=title,publishDate,generalDateOne&rPP=50&currentPage=1&currentUrl=https%3A%2F%2Ftargovishte.bg%2Fwps%2Fportal%2Fmunicipality-targovishte%2Factual%2Fevents%2F&dateFormat=dd.MM.yyyy&ancestors=false&descendants=true&orderBy=generalDateOne&orderBy2=publishDate&orderBy3=title&sortOrder=false&searchTerm=&filterByProperties=generalDateOne")
+    String getEvents();
 }
--- src/main/java/eu/trufchev/intargovishte/information/events/municipality/services/MunicipalityEventService.java
+++ src/main/java/eu/trufchev/intargovishte/information/events/municipality/services/MunicipalityEventService.java
@@ -29,43 +29,7 @@ public class MunicipalityEventService {
     }
 
     public String fetchEvents() {
-        return targovishteClient.getMunicipalityEvents(
-                "*/*",
-                "gzip, deflate, br, zstd",
-                "en-US,en;q=0.9",
-                "keep-alive",
-                "targovishte.bg",
-                "https://targovishte.bg/wps/portal/municipality-targovishte/actual/events",
-                "\"Google Chrome\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"",
-                "?0",
-                "\"macOS\"",
-                "empty",
-                "cors",
-                "same-origin",
-                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
-                "targovishte.bg1005",
-                "content",
-                "98cf5967-2cf7-4bda-9bf5-f41a08cc073a",
-                "cf1370df-43a1-4012-ba01-85607cf3fe92",
-                Arrays.asList("body", "summary", "startEventDate", "endEventDate", "eventType"),
-                "municipality-targovishte",
-                Arrays.asList("title", "publishDate", "generalDateOne"),
-                50,
-                1,
-                "https://targovishte.bg/wps/portal/municipality-targovishte/actual/events/",
-                "dd.MM.yyyy",
-                false,
-                true,
-                "generalDateOne",
-                "publishDate",
-                "title",
-                false,
-                "",
-                "",
-                "",
-                "generalDateOne",
-                ""
-        );
+        return targovishteClient.getEvents();
     }
 
     public List<MunicipalityEvent> parseAndSave() {
