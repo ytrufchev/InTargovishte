e75660f0cc387559d14c0853f0e984e412c76f4d
ytrufchev <yordan.tihomirov@gmail.com>
2024-11-27 13:00:27 +0200

    municipality events automation

--- src/main/java/eu/trufchev/intargovishte/information/events/municipality/feignClient/TargovishteClient.java
+++ src/main/java/eu/trufchev/intargovishte/information/events/municipality/feignClient/TargovishteClient.java
@@ -2,6 +2,7 @@ package eu.trufchev.intargovishte.information.events.municipality.feignClient;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(
         name = "targovishteClient",
@@ -9,6 +10,65 @@ import org.springframework.web.bind.annotation.GetMapping;
 )
 public interface TargovishteClient {
 
-    @GetMapping(value = "/customSearchWCM/query?context=targovishte.bg1005&libName=content&saId=98cf5967-2cf7-4bda-9bf5-f41a08cc073a&atId=cf1370df-43a1-4012-ba01-85607cf3fe92&returnElements=body,summary,startEventDate,endEventDate,eventType&rootPage=municipality-targovishte&returnProperties=title,publishDate,generalDateOne&rPP=50&currentPage=1&currentUrl=https%3A%2F%2Ftargovishte.bg%2Fwps%2Fportal%2Fmunicipality-targovishte%2Factual%2Fevents%2F&dateFormat=dd.MM.yyyy&ancestors=false&descendants=true&orderBy=generalDateOne&orderBy2=publishDate&orderBy3=title&sortOrder=false&searchTerm=&filterByProperties=generalDateOne")
-    String getEvents();
+    @GetMapping(value = "/customSearchWCM/query", headers = {
+            "Accept=*/*",
+            "Accept-Encoding=gzip, deflate, br, zstd",
+            "Accept-Language=en-US,en;q=0.9",
+            "Connection=keep-alive",
+            "Host=targovishte.bg",
+            "Referer=https://targovishte.bg/wps/portal/municipality-targovishte/actual/events",
+            "Sec-Ch-Ua=\"Google Chrome\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"",
+            "Sec-Ch-Ua-Mobile=?0",
+            "Sec-Ch-Ua-Platform=\"macOS\"",
+            "Sec-Fetch-Dest=empty",
+            "Sec-Fetch-Mode=cors",
+            "Sec-Fetch-Site=same-origin",
+            "User-Agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
+    })
+    String getEvents(
+            @RequestParam(name = "context") String context,
+            @RequestParam(name = "libName") String libName,
+            @RequestParam(name = "saId") String saId,
+            @RequestParam(name = "atId") String atId,
+            @RequestParam(name = "returnElements") String returnElements,
+            @RequestParam(name = "rootPage") String rootPage,
+            @RequestParam(name = "returnProperties") String returnProperties,
+            @RequestParam(name = "rPP") int rPP,
+            @RequestParam(name = "currentPage") int currentPage,
+            @RequestParam(name = "currentUrl") String currentUrl,
+            @RequestParam(name = "dateFormat") String dateFormat,
+            @RequestParam(name = "ancestors") boolean ancestors,
+            @RequestParam(name = "descendants") boolean descendants,
+            @RequestParam(name = "orderBy") String orderBy,
+            @RequestParam(name = "orderBy2") String orderBy2,
+            @RequestParam(name = "orderBy3") String orderBy3,
+            @RequestParam(name = "sortOrder") boolean sortOrder,
+            @RequestParam(name = "searchTerm") String searchTerm,
+            @RequestParam(name = "filterByProperties") String filterByProperties
+    );
+
+    // Overloaded method with no parameters, delegating to the parameterized method with default values
+    default String getEvents() {
+        return getEvents(
+                "targovishte.bg1005",
+                "content",
+                "98cf5967-2cf7-4bda-9bf5-f41a08cc073a",
+                "cf1370df-43a1-4012-ba01-85607cf3fe92",
+                "body,summary,startEventDate,endEventDate,eventType",
+                "municipality-targovishte",
+                "title,publishDate,generalDateOne",
+                50,
+                1,
+                "https://targovishte.bg/wps/portal/municipality-targovishte/actual/events/",
+                "dd.MM.yyyy",
+                false,
+                true,
+                "generalDateOne",
+                "publishDate",
+                "title",
+                false,
+                "",
+                "generalDateOne"
+        );
+    }
 }
