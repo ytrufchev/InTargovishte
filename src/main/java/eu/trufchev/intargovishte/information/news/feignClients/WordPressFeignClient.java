73d7cb1ade6533048c63c563ab1ac956be06f5ff
ytrufchev <yordan.tihomirov@gmail.com>
2024-12-12 13:40:08 +0200

    news source added

--- src/main/java/eu/trufchev/intargovishte/information/news/feignClients/WordPressFeignClient.java
+++ src/main/java/eu/trufchev/intargovishte/information/news/feignClients/WordPressFeignClient.java
@@ -2,11 +2,11 @@ package eu.trufchev.intargovishte.information.news.feignClients;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "wordpress-client", url = "https://targovishtebg.com/wp-json/wp/v2")
 public interface WordPressFeignClient {
 
     @GetMapping("/media/{postId}")
-    String getMediaDetails(@PathVariable("postId") Long postId);
+    String getMediaDetails(@RequestParam("parent") Long postId);
 }
