afa64f1cf9075916fa20215375d5a3eda1076fd8
ytrufchev <yordan.tihomirov@gmail.com>
2024-12-12 13:15:41 +0200

    news source added

--- src/main/java/eu/trufchev/intargovishte/information/news/feignClients/WordPressFeignClient.java
+++ src/main/java/eu/trufchev/intargovishte/information/news/feignClients/WordPressFeignClient.java
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface WordPressFeignClient {
 
     @GetMapping("/media/{postId}")
-    String getMediaDetails(@PathVariable("postId") int postId);
+    String getMediaDetails(@PathVariable("postId") Long postId);
 }
