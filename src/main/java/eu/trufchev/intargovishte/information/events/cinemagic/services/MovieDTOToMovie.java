d11b7d8bb7495a2553ac9e8341c10e8ac5dbeee7
ytrufchev <yordan.tihomirov@gmail.com>
2024-12-19 17:03:35 +0200

    like functionality

--- src/main/java/eu/trufchev/intargovishte/information/events/cinemagic/services/MovieDTOToMovie.java
+++ src/main/java/eu/trufchev/intargovishte/information/events/cinemagic/services/MovieDTOToMovie.java
@@ -2,6 +2,7 @@ package eu.trufchev.intargovishte.information.events.cinemagic.services;
 
 import eu.trufchev.intargovishte.information.events.cinemagic.dto.MovieDTO;
 import eu.trufchev.intargovishte.information.events.cinemagic.entities.Movie;
+import eu.trufchev.intargovishte.information.events.cinemagic.entities.MovieLike;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +12,7 @@ public class MovieDTOToMovie {
     public List<Movie> toMovie(List<MovieDTO> movieDTOs) {
         List<Movie> movies = new ArrayList<>();
         for (MovieDTO movieDTO : movieDTOs) {
+            List<MovieLike> movieLikes = new ArrayList<>();
             Movie movie = new Movie(
                     movieDTO.getId(),
                     movieDTO.getDuration(),
@@ -18,7 +20,8 @@ public class MovieDTOToMovie {
                     movieDTO.getTitle(),
                     movieDTO.getOriginalTitle(),
                     movieDTO.getIsForChildren(),
-                    movieDTO.getImdbId()
+                    movieDTO.getImdbId(),
+                    movieLikes
             );
             movies.add(movie);
         }
