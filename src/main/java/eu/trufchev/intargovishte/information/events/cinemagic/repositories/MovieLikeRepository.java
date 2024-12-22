356e6a96a08cb52d1a1bc7c4ee99d593df948954
ytrufchev <yordan.tihomirov@gmail.com>
2024-12-22 17:55:21 +0200

    PuppetTheaterController modifications

--- src/main/java/eu/trufchev/intargovishte/information/events/cinemagic/repositories/MovieLikeRepository.java
+++ src/main/java/eu/trufchev/intargovishte/information/events/cinemagic/repositories/MovieLikeRepository.java
@@ -16,6 +16,6 @@ public interface MovieLikeRepository extends JpaRepository<MovieLike, Long> {
     List<Movie> findByUserId(long userId);
     Optional<MovieLike> findByEventAndUser(Movie event, User user);
     boolean existsByEventAndUser(Movie event, User user);
-    void deleteByMovieId(String movieId);
+    void deleteByEventId(String movieId);
     long countByEvent(Movie event);
 }
