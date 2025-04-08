7417dcbf5eac2c989235dee08d2fecdb0c4fc981
ytrufchev <yordan.tihomirov@gmail.com>
2025-04-08 11:13:22 +0300

    Movie Like bug fix

--- src/main/java/eu/trufchev/intargovishte/user/repository/UserRepository.java
+++ src/main/java/eu/trufchev/intargovishte/user/repository/UserRepository.java
@@ -6,9 +6,10 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     Boolean existsByEmail(String email);
     Optional<User> findByUsernameOrEmail(String username, String email);
