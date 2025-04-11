cc36b183d09f0deb28390db90a36423ccd6b382c
ytrufchev <yordan.tihomirov@gmail.com>
2025-04-11 13:46:12 +0300

    Inapp information functionality added

--- src/main/java/eu/trufchev/intargovishte/information/inAppInformation/notifications/InfoNotificationClient.java
+++ src/main/java/eu/trufchev/intargovishte/information/inAppInformation/notifications/InfoNotificationClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "notificationClient", url = "https://api.telegram.org")
+@FeignClient(name = "infoNotificationClient", url = "https://api.telegram.org")
 public interface InfoNotificationClient {
 
     @GetMapping(value = "/{token}/sendMessage")
