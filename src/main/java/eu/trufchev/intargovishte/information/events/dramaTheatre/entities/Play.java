9e6886df14f5912fedacc7e937893345454bf0b0
ytrufchev <yordan.tihomirov@gmail.com>
2025-05-12 16:41:36 +0300

    plays update modification

--- src/main/java/eu/trufchev/intargovishte/information/events/dramaTheatre/entities/Play.java
+++ src/main/java/eu/trufchev/intargovishte/information/events/dramaTheatre/entities/Play.java
@@ -1,5 +1,6 @@
 package eu.trufchev.intargovishte.information.events.dramaTheatre.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import eu.trufchev.intargovishte.information.events.puppetTheatre.entities.PuppetTheaterLike;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -26,6 +27,7 @@ public class Play {
     private String placeName;
     private String startDates;
     @OneToMany(mappedBy = "event")
+    @JsonIgnore  // prevent infinite recursion
     private List<DramaLike> likes;
     public void setStartDates(List<String> startDatesList) {
         this.startDates = String.join(",", startDatesList);  // Converts list to a single string
