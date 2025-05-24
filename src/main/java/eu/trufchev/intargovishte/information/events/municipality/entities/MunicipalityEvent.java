c8d3d5856356da68ecb607aa4913bb3afad5b89e
ytrufchev <yordan.tihomirov@gmail.com>
2025-05-24 23:01:13 +0300

    Increased size for municipality event description

--- src/main/java/eu/trufchev/intargovishte/information/events/municipality/entities/MunicipalityEvent.java
+++ src/main/java/eu/trufchev/intargovishte/information/events/municipality/entities/MunicipalityEvent.java
@@ -19,6 +19,6 @@ public class MunicipalityEvent {
     private String date;
     @Column(name = "title")
     private String title;
-    @Column(name = "description", length = 2000)
+    @Column(name = "description", length = 5000)
     private String description;
 }
