800e9539c751bda223746631052033415e9bd94d
yordan <yordan.tihomirov@gmail.com>
2025-09-15 20:58:57 +0300

    profitshare test implementation

--- src/main/java/eu/trufchev/intargovishte/affiliate/ProfitshareFeignConfig.java
+++ src/main/java/eu/trufchev/intargovishte/affiliate/ProfitshareFeignConfig.java
@@ -2,16 +2,23 @@ package eu.trufchev.intargovishte.affiliate;
 
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
+import feign.codec.Decoder;
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.cloud.openfeign.support.ResponseEntityDecoder;
+import org.springframework.cloud.openfeign.support.SpringDecoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
-import java.util.Base64;
+import java.util.Collections;
 import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
@@ -66,5 +73,20 @@ public class ProfitshareFeignConfig {
             hex.append(String.format("%02x", b));
         }
         return hex.toString();
+    }
+
+    @Bean
+    public Decoder feignDecoder() {
+        ObjectFactory<HttpMessageConverters> objectFactory = () -> {
+            HttpMessageConverters converters = new HttpMessageConverters(
+                    new MappingJackson2HttpMessageConverter() {{
+                        setSupportedMediaTypes(Collections.singletonList(
+                                new MediaType("text", "json", StandardCharsets.UTF_8)
+                        ));
+                    }}
+            );
+            return converters;
+        };
+        return new ResponseEntityDecoder(new SpringDecoder(objectFactory));
     }
 }
