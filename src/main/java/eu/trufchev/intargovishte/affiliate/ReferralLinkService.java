26727e69614fb7a8a08e7be84279ab28da560c56
yordan <yordan.tihomirov@gmail.com>
2025-09-15 23:19:26 +0300

    profitshare test implementation

--- src/main/java/eu/trufchev/intargovishte/affiliate/ReferralLinkService.java
+++ src/main/java/eu/trufchev/intargovishte/affiliate/ReferralLinkService.java
@@ -3,6 +3,8 @@ package eu.trufchev.intargovishte.affiliate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+
 @Service
 @RequiredArgsConstructor
 public class ReferralLinkService {
@@ -10,7 +12,15 @@ public class ReferralLinkService {
     private final ProfitshareLinkClient linkClient;
 
     public String createReferralLink(String campaignUrl) {
-        ProfitshareDtos.ReferralLinkResponse response = linkClient.createLink(new ProfitshareDtos.ReferralLinkRequest(campaignUrl));
-        return response.result().short_link(); // Store the short link
+        // Create the LinkData object from the provided URL
+        ProfitshareDtos.LinkData linkData = new ProfitshareDtos.LinkData(null, campaignUrl);
+
+        // Create the ReferralLinkRequest with a list containing the single LinkData object
+        ProfitshareDtos.ReferralLinkRequest request = new ProfitshareDtos.ReferralLinkRequest(Collections.singletonList(linkData));
+
+        // Call the Feign client with the correctly formatted request object
+        ProfitshareDtos.ReferralLinkResponse response = linkClient.createLink(request);
+
+        return response.result().short_link(); // Return the short link
     }
 }
