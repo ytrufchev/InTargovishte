ff685c9b003ec86436e9d1c243c4eb65b8d90baf
yordan <yordan.tihomirov@gmail.com>
2025-09-16 11:05:52 +0300

    profitshare test implementation

--- src/main/java/eu/trufchev/intargovishte/affiliate/ProfitshareLinkClient.java
+++ src/main/java/eu/trufchev/intargovishte/affiliate/ProfitshareLinkClient.java
@@ -6,7 +6,11 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "profitshareLinks", url = "${profitshare.api-url}", configuration = ProfitshareFeignConfig.class)
+@FeignClient(
+        name = "profitshareLinks",
+        url = "${profitshare.api-url}",
+        configuration = ProfitshareFeignConfig.class // <-- Add this line
+)
 public interface ProfitshareLinkClient {
     @PostMapping(value = "/affiliate-links/", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     ProfitshareDtos.ReferralLinkResponse createLink(@RequestBody MultiValueMap<String, String> formData);
