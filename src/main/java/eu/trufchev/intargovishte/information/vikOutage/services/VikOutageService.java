ffe334c2d8e550e33f04a7cf3da9b47bd7b64e81
yordan <yordan.tihomirov@gmail.com>
2025-09-17 10:54:32 +0300

    profitshare test implementation

--- src/main/java/eu/trufchev/intargovishte/information/vikOutage/services/VikOutageService.java
+++ src/main/java/eu/trufchev/intargovishte/information/vikOutage/services/VikOutageService.java
@@ -8,6 +8,7 @@ import org.jsoup.nodes.Element;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -21,6 +22,12 @@ public class VikOutageService {
 
     public List<VikOutage> fetchAndParseVikOutage() {
         String htmlResponse = vikOutageClient.getOutage("15", "53", "50");
+        try {
+            byte[] bytes = htmlResponse.getBytes(StandardCharsets.ISO_8859_1);
+            htmlResponse = new String(bytes, "Windows-1251");
+        } catch (Exception e) {
+
+        }
         if (htmlResponse == null || htmlResponse.isBlank()) {
             return new ArrayList<>();
         }
